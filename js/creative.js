64a96823f985e229c9723bf4920a0de4f7efba74
Aitmukhammed <aitmukhammed_04@mail.ru>
2023-01-15 18:55:55 +0600

    15-Jan-23 End project

--- js/creative.js
+++ js/creative.js
@@ -1,94 +1,92 @@
 function openNav() {
-document.getElementById("mySidenav").style.width = "100%";
-}
-
-function closeNav() {
-document.getElementById("mySidenav").style.width = "0";
-}
-
-
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-  if(document.getElementById("demo").style.backgroundColor)
-  document.getElementById("demo").style.backgroundColor = "";
-  else
-    document.getElementById("demo").style.backgroundColor = "rgba(0,0,0,0.4)";
+  document.getElementById("mySidenav").style.width = "100%";
   }
-
-  window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn, .message, .submit_application')) {
-
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
+  
+  function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  }
+  
+  
+  function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+    if(document.getElementById("demo").style.backgroundColor)
+    document.getElementById("demo").style.backgroundColor = "";
+    else
+      document.getElementById("demo").style.backgroundColor = "rgba(0,0,0,0.4)";
     }
-  }
-}
   
-
-  window.onclick = function(event) {
-    if (!event.target.matches('.btndrop')) {
+    window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn, .message, .submit_application')) {
   
-      var dropdowns = document.getElementsByClassName("content-dropdown");
+      var dropdowns = document.getElementsByClassName("dropdown-content");
       var i;
       for (i = 0; i < dropdowns.length; i++) {
         var openDropdown = dropdowns[i];
       }
     }
   }
-
-
-function apple() {
-  document.getElementById("downdropmy").classList.toggle("showInfoCall");
-  if(document.getElementById("ss").style.backgroundColor)
-      document.getElementById("ss").style.backgroundColor = "";
-      else
-        document.getElementById("ss").style.backgroundColor = "red";
+    
+  
+    window.onclick = function(event) {
+      if (!event.target.matches('.btndrop')) {
+    
+        var dropdowns = document.getElementsByClassName("content-dropdown");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+          var openDropdown = dropdowns[i];
+        }
       }
-
-function closeAppli() {
-    document.getElementById("myDropdown").classList.remove('show');
-    if(document.getElementById("demo").style.backgroundColor )
-    document.getElementById("demo").style.backgroundColor = "";
-    else
-       document.getElementById("demo").style.backgroundColor = "rgba(0,0,0,0.4)";
     }
-
-function closeCall() {
-  document.getElementById("downdropmy").classList.remove('showInfoCall');
-  }   
-
-
-
-
-
-
-
-
-  var slideIndex = 1;
-  showSlides(slideIndex);
   
-  function plusSlides(n) {
-    showSlides(slideIndex += n);
-  }
   
-  function currentSlide(n) {
-    showSlides(slideIndex = n);
-  }
+  function apple() {
+    document.getElementById("downdropmy").classList.toggle("showInfoCall");
+    if(document.getElementById("ss").style.backgroundColor)
+        document.getElementById("ss").style.backgroundColor = "";
+        else
+          document.getElementById("ss").style.backgroundColor = "";
+        }
   
-  function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {slideIndex = 1}
-      if (n < 1) {slideIndex = slides.length}
-      for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+  function closeAppli() {
+      document.getElementById("myDropdown").classList.remove('show');
+      if(document.getElementById("demo").style.backgroundColor )
+      document.getElementById("demo").style.backgroundColor = "";
+      else
+         document.getElementById("demo").style.backgroundColor = "rgba(0,0,0,0.4)";
       }
-      for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-      }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
-  }
+  
+  function closeCall() {
+    document.getElementById("downdropmy").classList.remove('showInfoCall');
+    }   
+  
+  
+  
+  
+    const gap = 5;
+  
+  const carousel = document.getElementById("carousel"),
+    content = document.getElementById("content"),
+    next = document.getElementById("next"),
+    prev = document.getElementById("prev");
+  
+  next.addEventListener("click", e => {
+    carousel.scrollBy(width + gap, 0);
+    if (carousel.scrollWidth !== 0) {
+      prev.style.display = "flex";
+    }
+    if (content.scrollWidth - width - gap <= carousel.scrollLeft + width) {
+      next.style.display = "none";
+    }
+  });
+  prev.addEventListener("click", e => {
+    carousel.scrollBy(-(width + gap), 0);
+    if (carousel.scrollLeft - width - gap <= 0) {
+      prev.style.display = "none";
+    }
+    if (!content.scrollWidth - width - gap <= carousel.scrollLeft + width) {
+      next.style.display = "flex";
+    }
+  });
+  
+  let width = carousel.offsetWidth;
+  window.addEventListener("resize", e => (width = carousel.offsetWidth));
